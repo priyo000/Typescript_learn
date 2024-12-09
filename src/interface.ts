87522b091d2519c320bb581cfbe8a07fbebe2fa3
priyo000 <priyonewvky331@gmail.com>
2024-12-09 23:15:58 +0700

    learn readonly properties

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,7 @@
 export interface Seller{ //interface kurleb dengan alias namun lebih banyak digunakanoleh developer
     id: number,
     name: string,
-    category?: string
+    category?: string,
+    readonly nib: string,
+    readonly npwp?: string
 };
--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -5,7 +5,12 @@ describe('interface', () => {
         let seller: Seller = {
             id: 1,
             name: "Toko Mas Agung",
+            nib: "029129212",
+            npwp: "23232323223"
         }
+
+        seller.name = "Toko Mas Febri"; 
+        // seller.nib = "2323232"; //gak bisa diubah karena Readonly di properties interface
 
         console.info(seller);
     });
